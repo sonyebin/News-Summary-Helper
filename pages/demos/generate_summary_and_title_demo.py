831e687830b6dc98a08b88142d8b109327ce6d30
sonyebin <sonyeabin38@naver.com>
2025-05-11 01:56:03 +0900

    feat: 요약문 및 제목 생성 기능 구현
    입력에 따른 요약문 생성
    요약문에 따른 제목 생성
    생성된 요약문 및 제목 출력
    요약문 생성 실패시 경고 메시지

--- pages/demos/generate_summary_and_title_demo.py
+++ pages/demos/generate_summary_and_title_demo.py
@@ -1,15 +1,25 @@
 import streamlit as st
+from utils.summaryNewsTest import summary_news      # 요약문 생성
+from utils.generateTitleTest import generate_title  # 제목 생성
 
 
 def run():
     st.title("📝 본문 내용 요약문 및 제목 생성 Demo")
 
-    text = st.text_area("뉴스 본문을 입력하세요:", height=300)
+    text = st.text_area("뉴스 본문을 입력하세요", height=300)
 
     if st.button("Generate"):
         if not text.strip():
             st.warning("텍스트를 입력해주세요.")
         else:
             with st.spinner("생성 중입니다..."):
-                st.success("✅ 생성 완료 !")
-                st.write("To-Do")
+                summary = summary_news(text)
+                title = generate_title(summary)
+                if summary == "error1":
+                    st.warning("언어 감지에 실패하였습니다 (지원 언어: 영어, 한국어)")
+                elif summary == "error2":
+                    st.warning("지원하는 언어가 아닙니다 (지원 언어: 영어, 한국어)")
+                else:
+                    st.success("✅ 생성 완료 !")
+                    st.write(title)
+                    st.write(summary)
--- utils/summaryNewsTest.py
+++ utils/summaryNewsTest.py
@@ -1,22 +1,31 @@
-from transformers import BartForConditionalGeneration, PreTrainedTokenizerFast
+from langdetect import detect, LangDetectException  # 언어 감지 및 예외 처리
+import torch
+from transformers import pipeline  # 영어 기사 요약
+from transformers import BartForConditionalGeneration, PreTrainedTokenizerFast  # 한국어 기사 요약
 
 
-def summaryNews(text):
+def summary_news(text):
+    try:
+        lang = detect(text)      # 입력된 언어 감지
+    except LangDetectException:  # 언어 감지 실패 (너무 짧은 입력 or 지원언어 아님)
+        lang = None
 
-    model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
-    tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
+    if lang == 'en':             # 감지된 언어 영어
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        summary_eng = summarizer(text, max_length=256, min_length=64, do_sample=False)
+        summary = summary_eng[0]['summary_text']
+    elif lang == 'ko':           # 감지된 언어 한국어
+        sum_tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
+        sum_model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
 
-    inputs = tokenizer([text], max_length=1024, return_tensors="pt", truncation=True)
-    summary_ids = model.generate(
-        inputs['input_ids'],
-        max_length=128,
-        min_length=30,
-        num_beams=4,
-        length_penalty=2.0,
-        early_stopping=True
-    )
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        raw_input_ids = sum_tokenizer.encode(text)
+        input_ids = [sum_tokenizer.bos_token_id] + raw_input_ids + [sum_tokenizer.eos_token_id]
 
-    print("요약:", summary)
+        summary_ids = sum_model.generate(torch.tensor([input_ids]), max_length=256, min_length=64, num_beams=5)
+        summary = sum_tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+    elif not lang:               # 언어 감지에 실패한 경우
+        summary = "error1"
+    else:                        # 감지된 언어가 영어나 한국어가 아닐 경우 (ex. 일본어, 중국어 등)
+        summary = "error2"
+
     return summary
-
